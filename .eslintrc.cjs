02add234ef44d48bd41e56cedf252b256ef60394
stijnklomp <stijnklomp1@hotmail.com>
2024-01-21 17:17:06 +0000

    Add configuration for linting and formatting rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+	root: true,
+	extends: [
+		"./node_modules/stijnklomp-linting-formatting-config/eslintRules.js",
+		"./node_modules/stijnklomp-linting-formatting-config/jestRules.js",
+		"./node_modules/stijnklomp-linting-formatting-config/typescript/typescriptRules.js",
+		"prettier"
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "prettier"],
+	parserOptions: {
+		ecmaVersion: 2021,
+		sourceType: "module",
+		project: "./tsconfig.json"
+	},
+	env: {
+		browser: true,
+		node: true,
+		// commonjs: true,
+		es6: true
+	},
+	settings: {
+		"prettier/prettier": require("./node_modules/stijnklomp-linting-formatting-config/prettier/prettierRules.js")
+	},
+	rules: {
+		"prettier/prettier": 2,
+		quotes: ["error", "double"],
+		semi: [
+			"error",
+			"never",
+			{ beforeStatementContinuationChars: "always" }
+		],
+		"@stylistic/no-multiple-empty-lines": [
+			"error",
+			{ max: 1, maxBOF: 0, maxEOF: 0 }
+		]
+	}
+}
---
+++ .prettierrc.cjs
@@ -0,0 +1,1 @@
+module.exports = require("./node_modules/stijnklomp-linting-formatting-config/prettier/prettier")
